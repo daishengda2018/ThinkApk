24e2a2a8d0eeaec1408c47dcb27c590bdf6364a1
代圣达 <dsd13502@gmail.com>
2020-05-21 13:57:05 +0800

    init repository

---
+++ src/main/kotlin/com/mrcd/ThinkApkPlugin.kt
@@ -0,0 +1,16 @@
+package com.mrcd
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ *
+ * Create by im_dsd 2020/5/21 13:46
+ */
+class ThinkApkPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        TODO("Not yet implemented")
+    }
+
+}
