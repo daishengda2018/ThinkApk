87ce7840610df869bafd79eb69d6ee878bc9c2a2
代圣达 <dsd13502@gmail.com>
2020-05-21 17:19:48 +0800

    配置项目

--- src/main/kotlin/com/mrcd/ThinkApkPlugin.kt
+++ src/main/kotlin/com/mrcd/ThinkApkPlugin.kt
@@ -7,10 +7,15 @@ import org.gradle.api.Project
  *
  * Create by im_dsd 2020/5/21 13:46
  */
-class ThinkApkPlugin : Plugin<Project> {
+class ThinkApkPlugin :Plugin<Project> {
 
     override fun apply(project: Project) {
-        TODO("Not yet implemented")
+        project.tasks.create("thinkRes") {
+            it.doLast {
+                println("Hello from ThinkApkPlugin")
+            }
+        }.apply {
+            group = "think"
+        }
     }
-
 }
